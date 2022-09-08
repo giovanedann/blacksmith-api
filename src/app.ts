02986bc26ef747abe53e079cdac3d3a5ea3b9107
Giovane <giovanedaniell@gmail.com>
2022-09-08 20:54:44 -0300

    feat(routes): add product creation route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { Router } from 'express';
+import ProductController from './controllers/ProductController';
 
 const app = express();
+const routes = Router();
+
+routes.post('/products', ProductController.create);
 
 app.use(express.json());
+app.use(routes);
 
 export default app;
