d321f48d2f2f8d6586bee18d834194dc74ed4481
Giovane <giovanedaniell@gmail.com>
2022-09-08 19:52:51 -0300

    feat(services): create ProductsService and create method

---
+++ src/services/ProductsService.ts
@@ -0,0 +1,28 @@
+import { IProduct } from '../interfaces/Product';
+import ProductModel from '../models/ProductModel';
+import connection from '../models/connection';
+import { IMessage } from '../interfaces/Message';
+import { validateAmount, validateName } from '../utils/validateProducts';
+
+class ProductsService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async create({ name, amount }: IProduct): Promise<IProduct | IMessage> {
+    const nameWarning = validateName(name);
+    const amountWarning = validateAmount(amount);
+
+    if (nameWarning.message || amountWarning.message) {
+      return ({ message: nameWarning.message ?? amountWarning.message });
+    }
+
+    const createdBook = await this.model.create({ name, amount });
+
+    return createdBook;
+  }
+}
+
+export default ProductsService;
