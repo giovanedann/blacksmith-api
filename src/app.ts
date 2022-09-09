957118c1784946a3eb4dd37b039db1c6261ffa87
Giovane <giovanedaniell@gmail.com>
2022-09-09 19:32:08 -0300

    feat(routes): implement post user route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import ProductController from './controllers/ProductController';
+import UserController from './controllers/UserController';
 
 const app = express();
 const routes = Router();
 
 routes.get('/products', ProductController.index);
 routes.post('/products', ProductController.create);
+routes.post('/users', UserController.create);
 
 app.use(express.json());
 app.use(routes);
