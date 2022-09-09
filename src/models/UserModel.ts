470b0e0a71f7ce5707c4472fa95ab9fc54d91200
Giovane <giovanedaniell@gmail.com>
2022-09-09 19:29:21 -0300

    feat(models): create UserModel and create method

---
+++ src/models/UserModel.ts
@@ -0,0 +1,24 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IUser } from '../interfaces/User';
+import generateToken from '../utils/generateToken';
+
+class UserModel {
+  constructor(public connection: Pool) {}
+
+  public async create({ username, classe, level, password }: IUser): Promise<string> {
+    const query = `
+      INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);
+    `;
+
+    await this.connection.execute<ResultSetHeader>(
+      query,
+      [username, classe, level, password],
+    );
+
+    const token = generateToken(username, classe);
+
+    return token;
+  }
+}
+
+export default UserModel;
