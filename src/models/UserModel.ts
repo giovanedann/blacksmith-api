f6743c4f892577e1f80957bad0e9ae35882dd53a
Giovane <giovanedaniell@gmail.com>
2022-09-10 19:44:38 -0300

    feat(models): add findByLogin method on UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,10 +1,18 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
-import { IUser } from '../interfaces/User';
+import { IUser, ILogin } from '../interfaces/User';
 import generateToken from '../helpers/generateToken';
 
 class UserModel {
   constructor(public connection: Pool) {}
 
+  public async findByLogin({ username, password }: ILogin): Promise<IUser[] | []> {
+    const query = 'SELECT * FROM Trybesmith.Users WHERE username=(?) AND password=(?);';
+
+    const [user] = await this.connection.execute(query, [username, password]);
+
+    return user as IUser[];
+  }
+  
   public async create({ username, classe, level, password }: IUser): Promise<string> {
     const query = `
       INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);
@@ -15,7 +23,7 @@ class UserModel {
       [username, classe, level, password],
     );
 
-    const token = generateToken(username, classe);
+    const token = generateToken({ username });
 
     return token;
   }
