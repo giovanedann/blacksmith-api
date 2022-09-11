b7596faae5bb880ea9cf6409f4cdc6286b9e330e
Giovane <giovanedaniell@gmail.com>
2022-09-11 11:26:28 -0300

    feat(middlewares): create authorization middleware

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import connection from '../models/connection';
+import UserModel from '../models/UserModel';
+
+interface IDecodedJWT {
+  payload: {
+    id: number;
+    username: string;
+  };
+  [key: string]: any;
+}
+
+async function authorizate(request: Request, response: Response, next: NextFunction) {
+  const JWT_SECRET = 'segredofoda123@';
+  const { authorization } = request.headers;
+
+  const model = new UserModel(connection);
+
+  if (!authorization) return response.status(401).json({ message: 'Token not found' });
+
+  try {
+    const { payload } = verify(authorization, JWT_SECRET) as IDecodedJWT;
+    const [userExists] = await model.findById(payload.id);
+    if (userExists) next(); 
+  } catch (e) {
+    return response.status(401).json({ message: 'Invalid token' });
+  }
+}
+
+export default authorizate;
