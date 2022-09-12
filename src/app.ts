007e91bcc180ad481f6a16ebcd8da175d5a5bd46
Giovane <giovanedaniell@gmail.com>
2022-09-12 19:47:35 -0300

    feat(routes): create post /orders route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import OrderController from './controllers/OrderController';
 import ProductController from './controllers/ProductController';
 import UserController from './controllers/UserController';
+import authorizate from './middlewares/authorization';
 
 const app = express();
 const routes = Router();
@@ -11,6 +12,7 @@ routes.post('/products', ProductController.create);
 routes.post('/users', UserController.create);
 routes.post('/login', UserController.authenticate);
 routes.get('/orders', OrderController.index);
+routes.post('/orders', authorizate, OrderController.store);
 
 app.use(express.json());
 app.use(routes);
