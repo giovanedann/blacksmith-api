8c6853665cb34ee11904ea67c22385f15666b28f
Giovane <giovanedaniell@gmail.com>
2022-09-12 19:47:53 -0300

    feat: 100%

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 import connection from '../models/connection';
 import UserModel from '../models/UserModel';
 
-interface IDecodedJWT {
+export interface IDecodedJWT {
   payload: {
     id: number;
     username: string;
@@ -22,7 +22,9 @@ async function authorizate(request: Request, response: Response, next: NextFunct
   try {
     const { payload } = verify(authorization, JWT_SECRET) as IDecodedJWT;
     const [userExists] = await model.findById(payload.id);
-    if (userExists) next(); 
+    if (userExists) {
+      next(); 
+    }
   } catch (e) {
     return response.status(401).json({ message: 'Invalid token' });
   }
