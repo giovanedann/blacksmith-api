f9645610b811522f1d2a074e333eec639cdbc1d1
Giovane <giovanedaniell@gmail.com>
2022-09-13 18:13:30 -0300

    refactor(interfaces): add generics to IPayload interface

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -1,10 +1,10 @@
 import { sign } from 'jsonwebtoken';
 
-interface IPayload {
-  [payloadKey: string]: string | number;
+interface IPayload<T> {
+  [payloadKey: string]: T;
 }
 
-export default function generateToken(payload: IPayload): string {
+export default function generateToken<T>(payload: IPayload<T>): string {
   const JWT_SECRET = 'segredofoda123@';
 
   const token = sign({ payload }, JWT_SECRET, {
